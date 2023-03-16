0e8fade98ff07bbc15ca6084fbc88a2e00b12bc2
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-16 03:39:10 +0200

    Styled Header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
 
   const [navbar, setNavbar] = useState(false);
   return (
-    <div>
+    <div className="sti">
       <nav className="w-full bg-white shadow">
             <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
                 <div>
